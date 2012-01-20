d60785f5f07e7c6b9b582472b40b9b261f3a5150
Nur Hidayat <hidayat365@yahoo.com>
2012-01-20 23:22:18 +0700

    modify config
    Signed-off-by: Nur Hidayat <hidayat365@yahoo.com>

--- trunk/protected/config/main.php
+++ trunk/protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Kelsri ERP',
+	'name'=>'Biruni ERP',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
