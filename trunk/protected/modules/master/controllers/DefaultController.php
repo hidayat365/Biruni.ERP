ecf0ea19d17748d3fdfaa743cebbaf8023819dc2
Nur Hidayat <hidayat365@yahoo.com>
2012-05-01 01:28:10 +0700

    commit
    Signed-off-by: Nur Hidayat <hidayat365@yahoo.com>

--- trunk/protected/modules/master/controllers/DefaultController.php
+++ trunk/protected/modules/master/controllers/DefaultController.php
@@ -2,6 +2,18 @@
 
 class DefaultController extends BiruniController
 {
+	public $adminMenu = array(
+		array('label'=>'Manage Branches', 'url'=>array('/master/branch')),
+		array('label'=>'Manage Departments', 'url'=>array('/master/department')),
+		array('label'=>'Manage Projects', 'url'=>array('/master/project')),
+		array('label'=>'Manage Customers', 'url'=>array('/master/company/customer')),
+		array('label'=>'Manage Suppliers', 'url'=>array('/master/company/supplier')),
+		array('label'=>'Manage Accounts', 'url'=>array('/master/account')),
+		array('label'=>'Manage Item Categories', 'url'=>array('/master/itemcategory')),
+		array('label'=>'Manage Items', 'url'=>array('/master/item')),
+		array('label'=>'Manage Order Categories', 'url'=>array('/master/ordercategory')),
+	);
+
 	/**
 	 * @return array action filters
 	 */
--- trunk/themes/shadow_dancer/views/site/index.php
+++ trunk/themes/shadow_dancer/views/site/index.php
@@ -13,9 +13,14 @@
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i> Dashboard</h1>
 
+<!--
 <div class="flash-error">This is an example of an error message to show you that things have gone wrong.</div>
 <div class="flash-notice">This is an example of a notice message.</div>
 <div class="flash-success">This is an example of a success message to show you that things have gone according to plan.</div>
+-->
+<div class="flash-success">Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i> Dashboard</div>
+<div class="flash-notice">This is very very early version and not fully functional yet</div>
+
 <div class="span-23 showgrid">
 
 <div class="span-10">
